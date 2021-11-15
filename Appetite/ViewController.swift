b941c3b4ed2e091dfce1b18d89b1500cd26005cb
ufuk-trkz <ufuk_trkz@hotmail.de>
2021-11-15 19:59:00 +0100

    first commit

---
+++ Appetite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Appetite
+//
+//  Created by Ufuk Türközü on 15.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
