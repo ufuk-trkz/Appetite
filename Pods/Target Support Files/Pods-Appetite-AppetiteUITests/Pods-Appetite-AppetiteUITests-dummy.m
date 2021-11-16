5cdfe64cd26c4ef7e619ca5744ad327cdfc7e421
ufuk-trkz <ufuk_trkz@hotmail.de>
2021-11-16 21:39:29 +0100

    add Firebase

---
+++ Pods/Target Support Files/Pods-Appetite-AppetiteUITests/Pods-Appetite-AppetiteUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Appetite_AppetiteUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Appetite_AppetiteUITests
+@end
---
+++ Pods/Target Support Files/Pods-AppetiteTests/Pods-AppetiteTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppetiteTests : NSObject
+@end
+@implementation PodsDummy_Pods_AppetiteTests
+@end
